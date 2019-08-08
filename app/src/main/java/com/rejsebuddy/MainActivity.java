c43a1072e899897721d7aca28e2ae52df8af36d1
Andreas <thecodingbeast@gmail.com>
2019-08-08 19:51:11 +0200

    Added wip settings pane

--- app/src/main/java/com/rejsebuddy/MainActivity.java
+++ app/src/main/java/com/rejsebuddy/MainActivity.java
@@ -3,16 +3,23 @@ package com.rejsebuddy;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Initiate the activity.
+     *
+     * @param state The previous state.
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        // Create the instance.
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle state) {
+        // Initiate the content view.
+        super.onCreate(state);
         setContentView(R.layout.activity_main);
 
         // Connect page adapter to view element.
@@ -22,5 +29,14 @@ public class MainActivity extends AppCompatActivity {
         // Connect tab layout to view element.
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(pager);
+    }
+
+    /**
+     * Starts the settings page activity.
+     *
+     * @param view The current view
+     */
+    public void openSettings(View view) {
+        startActivity(new Intent(this, SettingsActivity.class));
     }
 }
---
+++ app/src/main/java/com/rejsebuddy/SettingsActivity.java
@@ -0,0 +1,43 @@
+package com.rejsebuddy;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.MenuItem;
+import android.view.View;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    /**
+     * Initiate the activity.
+     *
+     * @param state The previous state.
+     */
+    @Override
+    protected void onCreate(Bundle state) {
+        // Initiate the content view.
+        super.onCreate(state);
+        setContentView(R.layout.activity_settings);
+
+        // Enable the return button.
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    /**
+     * Return back to previous view on click.
+     *
+     * @return Whether or not an action was performed.
+     */
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+}
