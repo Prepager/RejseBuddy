aadb885a68c321e5657fa8c5563343cc449b4ef5
Andreas <andreasfrendorf@gmail.com>
2019-08-14 19:22:19 +0200

    Prepared API consumer with locations endpoint handler

---
+++ app/src/main/java/com/rejsebuddy/api/Client.java
@@ -0,0 +1,68 @@
+package com.rejsebuddy.api;
+
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+class Client {
+
+    /**
+     * The base url of the api interface.
+     */
+    private String baseURL = "http://xmlopen.rejseplanen.dk/bin/rest.exe";
+
+    /**
+     * Fetch a passed URL address.
+     *
+     * @param address The address to be fetched.
+     * @return The fetched input stream.
+     */
+    private InputStream fetch(String address) throws IOException {
+        // Create new request URL address.
+        URL url = new URL(this.baseURL + address);
+
+        // Create new connection and connect.
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.connect();
+
+        // Return the connection input stream.
+        return connection.getInputStream();
+    }
+
+    /**
+     * Parse an incoming XML input stream.
+     *
+     * @param stream The http input stream.
+     * @return The XML parser instance.
+     */
+    private XmlPullParser parse(InputStream stream) throws XmlPullParserException, IOException {
+        // Create new parser and set input stream.
+        XmlPullParser parser = Xml.newPullParser();
+        //parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+        parser.setInput(stream, null);
+        parser.nextTag();
+
+        // Close the input stream.
+        stream.close();
+
+        // Return the created parser.
+        return parser;
+    }
+
+    /**
+     * Fetch and parse the requested URL address.
+     *
+     * @param address The URL address to be parsed.
+     * @return The XML parser instance.
+     */
+    public XmlPullParser request(String address) throws XmlPullParserException, IOException {
+        return this.parse(this.fetch(address));
+    }
+
+}
---
+++ app/src/main/java/com/rejsebuddy/api/Consumer.java
@@ -0,0 +1,19 @@
+package com.rejsebuddy.api;
+
+import org.xmlpull.v1.XmlPullParser;
+
+import java.net.URLEncoder;
+
+public class Consumer extends Client {
+
+    /**
+     * Requests location endpoint for input.
+     *
+     * @param input The query input string.
+     * @return The XML parser instance.
+     */
+    public XmlPullParser requestLocation(String input) throws Exception {
+        return this.request("/location?input=" + URLEncoder.encode(input, "UTF-8"));
+    }
+
+}
