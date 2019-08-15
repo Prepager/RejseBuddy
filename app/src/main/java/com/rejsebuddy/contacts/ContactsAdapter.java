4fe8cf99a82322070d6ea760dd4571b54eee0671
Andreas <andreasfrendorf@gmail.com>
2019-08-15 19:13:55 +0200

    Extracted contacts row holder to own class file and cleaned up

--- app/src/main/java/com/rejsebuddy/contacts/ContactsAdapter.java
+++ app/src/main/java/com/rejsebuddy/contacts/ContactsAdapter.java
@@ -1,10 +1,8 @@
 package com.rejsebuddy.contacts;
 
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,7 +11,7 @@ import com.rejsebuddy.storage.contact.Contact;
 
 import java.util.List;
 
-public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ContactViewHolder> {
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsRowHolder> {
 
     /**
      * The list of passed contacts.
@@ -37,13 +35,13 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.Contac
      * @return View holder of the contact row.
      */
     @Override
-    public ContactViewHolder onCreateViewHolder(ViewGroup parent, int type) {
+    public ContactsRowHolder onCreateViewHolder(ViewGroup parent, int type) {
         // Inflate the contact row view.
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View view = inflater.inflate(R.layout.row_contact, parent, false);
 
         // Return new view holder instance.
-        return new ContactViewHolder(view);
+        return new ContactsRowHolder(view);
     }
 
     /**
@@ -53,10 +51,8 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.Contac
      * @param index The index of the current view.
      */
     @Override
-    public void onBindViewHolder(ContactViewHolder holder, int index) {
-        holder.id = contacts.get(index).getId();
-        holder.name.setText(contacts.get(index).getName());
-        holder.address.setText(contacts.get(index).getAddress());
+    public void onBindViewHolder(ContactsRowHolder holder, int index) {
+        holder.setContact(contacts.get(index));
     }
 
     /**
@@ -67,69 +63,6 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.Contac
     @Override
     public int getItemCount() {
         return contacts.size();
-    }
-
-    /**
-     * Wrapper class to hold contact view elements.
-     */
-    public class ContactViewHolder extends RecyclerView.ViewHolder {
-
-        /**
-         * The contact model id.
-         */
-        private int id;
-
-        /**
-         * The contact name text element.
-         */
-        final private TextView name;
-
-        /**
-         * The contact address text element.
-         */
-        final private TextView address;
-
-        /**
-         * Populates the layout element holders.
-         *
-         * @param view The contact row view.
-         */
-        ContactViewHolder(View view) {
-            // Call view holder super.
-            super(view);
-
-            // Bind the view elements.
-            this.name = view.findViewById(R.id.name);
-            this.address = view.findViewById(R.id.address);
-
-            // Add listener to on click event.
-            view.setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View v) {
-                    // TODO
-                    System.out.println("Single tap " + getAdapterPosition());
-                }
-
-            });
-
-            // Add listener to on long click event.
-            view.setOnLongClickListener(new View.OnLongClickListener() {
-
-                @Override
-                public boolean onLongClick(View v) {
-                    // Start new intent and include contact id.
-                    Intent intent = new Intent(v.getContext(), ContactsEditorActivity.class);
-                    intent.putExtra("CONTACT_ID", id);
-                    v.getContext().startActivity(intent);
-
-                    // Mark action as handled.
-                    return true;
-                }
-
-            });
-        }
-
     }
 
 }
---
+++ app/src/main/java/com/rejsebuddy/contacts/ContactsRowHolder.java
@@ -0,0 +1,86 @@
+package com.rejsebuddy.contacts;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.rejsebuddy.R;
+import com.rejsebuddy.storage.contact.Contact;
+
+class ContactsRowHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+
+    /**
+     * The contact model id.
+     */
+    private int id;
+
+    /**
+     * The contact name text element.
+     */
+    final private TextView name;
+
+    /**
+     * The contact address text element.
+     */
+    final private TextView address;
+
+    /**
+     * Populates the layout element holders.
+     *
+     * @param view The contact row view.
+     */
+    ContactsRowHolder(View view) {
+        // Call view holder super.
+        super(view);
+
+        // Bind the view elements.
+        this.name = view.findViewById(R.id.name);
+        this.address = view.findViewById(R.id.address);
+
+        // Bind single and long click listeners.
+        view.setOnClickListener(this);
+        view.setOnLongClickListener(this);
+    }
+
+    /**
+     * Bind the row single click event.
+     *
+     * @param view The current view.
+     */
+    public void onClick(View view) {
+        // TODO
+        System.out.println("Single tap " + getAdapterPosition());
+    }
+
+    /**
+     * Bind the row long click event.
+     *
+     * @param view The current view.
+     */
+    public boolean onLongClick(View view) {
+        // Get context from view.
+        Context ctx = view.getContext();
+
+        // Start new intent and include contact id.
+        Intent intent = new Intent(ctx, ContactsEditorActivity.class);
+        intent.putExtra("CONTACT_ID", id);
+        ctx.startActivity(intent);
+
+        // Mark action as handled.
+        return true;
+    }
+
+    /**
+     * Sets the contact details on the view.
+     *
+     * @param contact The list row contact.
+     */
+    public void setContact(Contact contact) {
+        this.id = contact.getId();
+        this.name.setText(contact.getName());
+        this.address.setText(contact.getAddress());
+    }
+}
